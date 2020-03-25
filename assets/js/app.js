08c7b6c84cd609cba974f1b18e2e87caa2358671
Fernando Saavedra <yugi-fer@hotmail.com>
2020-03-24 23:44:08 -0600

    player turn and add card to dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,18 @@
  * XXD = DIAMONDS (DIAMANTES)
  * XXS = SPADES (ESPADAS)
  */
-
+const NEWGAME = document.querySelector('#newGame');
+const REQUESTC = document.querySelector('#requestC');
+const STOP = document.querySelector('#stop');
+const PLAYERSMALL = document.querySelectorAll('small')[0];
+const PCSMALL = document.querySelectorAll('small')[1];
+const PLAYERCARDS = document.querySelector('#player-cards');
+const PCCARDS = document.querySelector('#computer-cards');
 let deck = [];
 const types = ['C', 'D', 'H', 'S']; //tipos de cartas
 const figures = ['A', 'J', 'Q', 'K']; //figuras existentes
+let playerPoints = 0;
+let pcPoints = 0;
 const createDeck = () => { //se crea una baraja
 
     for (let i = 2; i <= 10; i++) //creamos las cartas numericas
@@ -19,7 +27,7 @@ const createDeck = () => { //se crea una baraja
             deck.push(`${figure}${type}`)
 
     deck = _.shuffle(deck); //revolvemos deck
-    console.log(JSON.stringify(deck));
+
     return deck; //regresamos deck
 };
 
@@ -27,21 +35,59 @@ const requestCard = () => { //pedir una carta
     if (deck.length === 0) {
         throw 'No hay cartas en el deck';
     }
-    const carta = deck.pop();
-    console.log(JSON.stringify(deck));
-    console.log(carta);
-    return carta;
+    const card = deck.pop();
+
+    return card;
 }
 
-const cardValue = (carta) => {
-    const valor = carta.substring(0, carta.length - 1);
-    console.log({ carta, valor });
-    return (isNaN(valor)) ? ((valor === 'A') ? 11 : 10) 
-    :(parseInt(valor));
+const cardValue = (card) => {
+    const value = card.substring(0, card.length - 1);
+    return (isNaN(value)) ? ((value === 'A') ? 11 : 10)
+        : (parseInt(value));
 
 
 }
+const createCardIMG = (card)=> {
+    const cardImg = document.createElement('img');
+    cardImg.src = `assets/cards/${card}.png`;
+    cardImg.classList.add('cards');
+    return cardImg
+}
+const reset = ()=>{
+    playerPoints = 0;
+    pcPoints = 0;
+    PLAYERSMALL.innerHTML = playerPoints;
+    PCSMALL.innerHTML = pcPoints;
+    PCCARDS.innerHTML="";
+    PLAYERCARDS.innerHTML="";
+    deck = [];
+    createDeck();
+    REQUESTC.disabled = false;
+    STOP.disabled = false;
+    NEWGAME.disabled = false;false
+}
+
+const endGame = (points)=>{
+    if(playerPoints ===21){
+        console.warn('ganaste!');
+        REQUESTC.disabled = true;
+        STOP.disabled = true;
+    }
+    if (playerPoints > 21){
+        console.warn('Lo siento, perdiste');
+        REQUESTC.disabled = true;
+        STOP.disabled = true;
+    }
+}
+REQUESTC.addEventListener('click', () => {
+    const card = requestCard();
+    playerPoints += cardValue(card);
+    PLAYERCARDS.append(createCardIMG(card));
+    PLAYERSMALL.innerHTML = playerPoints;
+    endGame(playerPoints);
+
+});
+NEWGAME.addEventListener('click',reset);
+
+
 createDeck();
-
-
-console.log(cardValue(requestCard()));
