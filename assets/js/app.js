3e7b1c3fdcb6aa61d8b57190c1d9ef78e64ce110
Fernando Saavedra <yugi-fer@hotmail.com>
2020-03-25 00:12:45 -0600

    pc turn win and reset

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,47 +47,68 @@ const cardValue = (card) => {
 
 
 }
-const createCardIMG = (card)=> {
+const createCardIMG = (card) => {
     const cardImg = document.createElement('img');
     cardImg.src = `assets/cards/${card}.png`;
     cardImg.classList.add('cards');
     return cardImg
 }
-const reset = ()=>{
+const reset = () => {
     playerPoints = 0;
     pcPoints = 0;
     PLAYERSMALL.innerHTML = playerPoints;
     PCSMALL.innerHTML = pcPoints;
-    PCCARDS.innerHTML="";
-    PLAYERCARDS.innerHTML="";
+    PCCARDS.innerHTML = "";
+    PLAYERCARDS.innerHTML = "";
     deck = [];
     createDeck();
     REQUESTC.disabled = false;
     STOP.disabled = false;
-    NEWGAME.disabled = false;false
+    NEWGAME.disabled = false;
 }
 
-const endGame = (points)=>{
-    if(playerPoints ===21){
-        console.warn('ganaste!');
-        REQUESTC.disabled = true;
-        STOP.disabled = true;
+const endGame = () => {
+    REQUESTC.disabled = true;
+    STOP.disabled = true;
+    if (playerPoints === pcPoints) {
+        console.warn('Empate!!!');
+    } else if (playerPoints > 21) {
+        console.warn('perdiste!!!');
+    } else if (pcPoints > 21) {
+        console.warn('Ganaste!!');
+    } else if (pcPoints > playerPoints) {
+        console.warn('perdiste!!!');
     }
-    if (playerPoints > 21){
-        console.warn('Lo siento, perdiste');
-        REQUESTC.disabled = true;
-        STOP.disabled = true;
-    }
+
+}
+const computerTurn = (minimalPoints) => {
+    do {
+        const card = requestCard();
+        pcPoints += cardValue(card);
+        PCCARDS.append(createCardIMG(card));
+        PCSMALL.innerHTML = pcPoints;
+        if (minimalPoints > 21) {
+            break;
+        }
+    } while ((pcPoints <= minimalPoints) && (pcPoints <= 21));
+    endGame();
+
 }
 REQUESTC.addEventListener('click', () => {
     const card = requestCard();
     playerPoints += cardValue(card);
     PLAYERCARDS.append(createCardIMG(card));
     PLAYERSMALL.innerHTML = playerPoints;
-    endGame(playerPoints);
+    if (playerPoints > 21) {
+        computerTurn();
+    }
 
 });
-NEWGAME.addEventListener('click',reset);
+NEWGAME.addEventListener('click', reset);
 
+STOP.addEventListener('click', () => {
+    computerTurn(playerPoints);
+
+})
 
 createDeck();
