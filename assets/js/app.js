db9964f36f0bc2ee9b3ffeccebc70c6ec51155f1
Fernando Saavedra <yugi-fer@hotmail.com>
2020-03-24 22:50:13 -0600

    request card and card value

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,21 +7,41 @@
 
 let deck = [];
 const types = ['C', 'D', 'H', 'S']; //tipos de cartas
-const figures = ['A','J','Q','K']; //figuras existentes
+const figures = ['A', 'J', 'Q', 'K']; //figuras existentes
 const createDeck = () => { //se crea una baraja
 
     for (let i = 2; i <= 10; i++) //creamos las cartas numericas
-        for(let type of types)
+        for (let type of types)
             deck.push(`${i}${type}`);
-        
-    for(let type of types) //creamos las figuras
-        for(let figure of figures)
+
+    for (let type of types) //creamos las figuras
+        for (let figure of figures)
             deck.push(`${figure}${type}`)
-        
+
     deck = _.shuffle(deck); //revolvemos deck
+    console.log(JSON.stringify(deck));
     return deck; //regresamos deck
 };
-const requestCard = ()=>{
+
+const requestCard = () => { //pedir una carta 
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    console.log(JSON.stringify(deck));
+    console.log(carta);
+    return carta;
+}
+
+const cardValue = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    console.log({ carta, valor });
+    return (isNaN(valor)) ? ((valor === 'A') ? 11 : 10) 
+    :(parseInt(valor));
+
 
 }
 createDeck();
+
+
+console.log(cardValue(requestCard()));
